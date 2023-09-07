3dbc745cfc286636c6125d79b4fbd0782506be8f
Nick <120607073+snydernb1@users.noreply.github.com>
2023-09-07 14:07:08 -0600

    Time: 47 ms (83.76%), Space: 42.3 MB (19.53%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -8,14 +8,36 @@ var lengthOfLastWord = function(s) {
     // iterate backwards till the index has length 
     // return length
     
-    let arr = s.split(' ')
+//     let arr = s.split(' ')
     
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i].length) {
-            return arr[i].length
-        }
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         if (arr[i].length) {
+//             return arr[i].length
+//         }
+//     }
+    
+    // Declare a count variable
+    // Iterate through the string backwards
+    // once we get to a non empty character start counting 
+    // return count once we hit another empty string character
+    
+    let count = 0
+
+    
+     for (let i = s.length - 1; i >= 0; i--) {
+         
+         if (count > 0 && s[i] === " ") {
+             return count
+         }
+         
+         if (s[i] !== " ") {
+             count++
+         }
+         
+     
     }
     
-    // 
+    return count
+    
     
 };

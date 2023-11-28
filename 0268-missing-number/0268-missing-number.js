daf3caacfdca05ace3fca10557bb4410622cb05e
Nick <120607073+snydernb1@users.noreply.github.com>
2023-11-28 09:44:19 -0700

    Time: 51 ms (88.60%), Space: 48.1 MB (8.29%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+    let numSet = new Set(nums)
+
+    for (let i = 0; i < nums.length + 1; i++) {
+        
+        if (!numSet.has(i)) {
+            return i
+        }
+    }
+    
+};
